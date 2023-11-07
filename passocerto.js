35886f9ae1c6b0307fa820ac830a2718ee275003
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 13:41:17 -0300

    teste

--- passocerto.js
+++ passocerto.js
@@ -1,97 +1,100 @@
-//instalando programas
+//requisitando os modulos
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
-//configurando o roteamento para teste no postman
+//configurando o express para o postman e para usar a pagina
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 const port = 3000;
 
-
-//configurando o acesso ao mongodb
-mongoose.connect('mongodb://127.0.0.1:27017/passocerto',
-{   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS : 20000
+//configurando o banco de dados
+mongoose.connect("mongodb://127.0.0.1:27017/passocerto", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-
-//criando a model do seu projeto
-const usuario = new mongoose.Schema({
-    email : {type : String, required: true},
+//criando a model usuario do meu projeto
+const UsuarioSchema = new mongoose.Schema({
+    email : {type : String, required : true},
     senha : {type : String}
 });
 
-const Pessoa = mongoose.model("Pessoa", usuario);
-
-//configurando os roteamentos
-app.post("/cadastropessoa", async(req, res)=>{
-    const email = req.body.email;
-    const senha = req.body.senha;
-
-    const pessoa = new Pessoa({
-        email : email,
-        senha : senha,
-    })
-
-
-    try{
-        const newPessoa = await pessoa.save();
-        res.json({error : null, msg : "Cadastro ok", pessoaId : newPessoa._id});
-    } catch(error){
-        res.status(400).json({error});
-    }
-
-
-});
-
-
-//criando a model do seu projeto
-const produtocalcado = new mongoose.Schema({
-    id_produtocalcado : {type : String, required: true},
+const ProdutocalcadoSchema = new mongoose.Schema({
+    id_produtocalcado : {type : String, required : true},
     descricao : {type : String},
-    marca : {type: String},
-    Data_fabricacao : {type : String},
-    Quantidade_estoque : {type : String}
+    marca : {type : String},
+    datafabricacao : {type : Date},
+    quantidadeestoque : {type : Number}
+})
+
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
+
+const Produtocalcado = mongoose.model("Produtocalcado",ProdutocalcadoSchema)
+
+//configuração dos roteamendos
+//cadastrousuario
+app.post("/cadastrousuario", async (req, res) => {
+  const email = req.body.email;
+  const senha = req.body.senha;
+  
+   
+  const usuario = new Usuario({
+    email: email,
+    senha: senha
 });
 
-const Calcado = mongoose.model("Calcado", produtocalcado);
+  try {
+    const newUsuario = await usuario.save();
+    res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+  } catch (error) {}
 
-//configurando os roteamentos
-app.post("/cadastroproduto", async(req, res)=>{
+});
+
+
+//rota de cadastro especifico
+app.post("/cadastroprodutocalcado", async (req, res) => {
+    
+    
     const id_produtocalcado = req.body.id_produtocalcado;
     const descricao = req.body.descricao;
-    const marca  = req.body.marca;
-    const Data_fabricacao = req.body.Data_fabricacao;
-    const Quantidade_estoque = req.body.Quantidade_estoque
-
+    const marca = req.body.marca;
+    const datafabricacao = req.body.datafabricacao;
+    const quantidadeestoque = req.body.quantidadeestoque;
+     
     const calcado = new Calcado({
-        id_produtocalcado : id_produtocalcado,
-        descricao : descricao,
-        marca : marca,
-        Data_fabricacao : Data_fabricacao,
-        Quantidade_estoque : Quantidade_estoque
-    })
+    id_produtocalcado: id_produtocalcado,
+    descricao: descricao,
+    marca: marca,
+    datafabricacao: datafabricacao,
+    quantidadeestoque: quantidadeestoque
+    });
+  
+    try {
+      const newProdutocalcado = await id_produtocalcado.save();
+      res.json({ error: null, msg: "Cadastro ok", ProdutocalcadoId: newProdutocalcado._id });
+    } catch (error) {}
+  
+  });
 
-    try{
-        const newCalcado = await calcado.save();
-        res.json({error : null, msg : "Cadastro ok", calcadoId : newCalcado._id});
-    } catch(error){
-        res.status(400).json({error});
-    }
-
-
+//rota padrao
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/cadastrousuario.html");
 });
 
-app.get("/", async(req, res)=>{
-    res.sendFile(__dirname +"/index.html");
-})
+app.get("/", async (req, res) => {
+    res.sendFile(__dirname + "/cadastrousuario.html");
+  });
+  
 
+app.get("/", async (req, res) => {
+    res.sendFile(__dirname + "/cadastroprodutocalcado.html");
+  });
+  
 
-//configurando a porta
-app.listen(port, ()=>{
-    console.log(`Servidor rodando na porta ${port}`);
-})
+//tem que ter o comando de listen
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
